0fb89f53b6b890fcced24430f111211919d5ec3b
Ian Rodriguez <ian.rgz@gmail.com>
2012-06-05 12:15:02 -0700

    Changed configuration and tests

--- test/unit/border_client_test.rb
+++ test/unit/border_client_test.rb
@@ -4,14 +4,14 @@ require 'borderline'
 module BorderLine
   class ClientTest < MiniTest::Unit::TestCase
     def test_client
-      response = Client.fetch "http://apps.cbp.gov/bwt/bwt.xml"
+      response = Client.fetch_borders "http://apps.cbp.gov/bwt/bwt.xml"
       assert_operator response.length, :>, 0
     end
   end
   
   class GeocodeTest < MiniTest::Unit::TestCase
     def test_geolocator
-      data =  Geocode.fecth_location '201.166.59.175'
+      data =  Geocode.fecth_location 'some_ip'
       assert data, "Data is not nil."
     end
   end
